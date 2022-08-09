b2d60a2fa3ddf45bcb58e3e90ac8d20c9fffb6d9
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-08-09 17:31:46 +0300

    refactoring

--- RestApp/src/main/java/com/senla/config/WebSecurityConfig.java
+++ RestApp/src/main/java/com/senla/config/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) throws Exception{
         web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
                 "/swagger-resources/**", "/configuration/**",
                 "/swagger-ui.html", "/webjars/**");
