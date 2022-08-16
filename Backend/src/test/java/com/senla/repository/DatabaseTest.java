7a61299878dde74ddcf2f3bd05f5dd8fc0d8be3a
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-08-16 13:21:42 +0300

    fix testcontainers

--- Backend/src/test/java/com/senla/repository/DatabaseTest.java
+++ Backend/src/test/java/com/senla/repository/DatabaseTest.java
@@ -2,6 +2,7 @@ package com.senla.repository;
 
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,23 +11,23 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @ContextConfiguration(initializers = DatabaseTest.DataSourceInitializer.class)
+@DirtiesContext
 public abstract class DatabaseTest {
 
     @Container
-    static final PostgreSQLContainer<?> database =
-            new PostgreSQLContainer<>("postgres:12.9-alpine");
+    private static final PostgreSQLContainer<?> DATABASE =
+            new PostgreSQLContainer<>("postgres:latest");
 
-    public static class DataSourceInitializer
+    static class DataSourceInitializer
             implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
             TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
                     applicationContext,
                     "spring.test.database.replace=none",
-                    "spring.datasource.url=" + database.getJdbcUrl(),
-                    "spring.datasource.username=" + database.getUsername(),
-                    "spring.datasource.password=" + database.getPassword());
+                    "spring.datasource.url=" + DATABASE.getJdbcUrl(),
+                    "spring.datasource.username=" + DATABASE.getUsername(),
+                    "spring.datasource.password=" + DATABASE.getPassword());
         }
     }
 }
